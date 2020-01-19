b9a814fa8990322cfcfa0d78d3122eeb967c3280
Alquamah <alqamahjsr01@gmail.com>
2020-01-19 13:39:49 +0530

    First commit

---
+++ build/precache-manifest.679d3f27151d018df70a856a5b51511b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc263c3c10f41083a592709c3e054961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d1467baaf2d6f6dd51",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "b520bfd30b3b046c8f51",
+    "url": "/static/css/main.20034467.chunk.css"
+  },
+  {
+    "revision": "e0d1467baaf2d6f6dd51",
+    "url": "/static/js/2.207066f5.chunk.js"
+  },
+  {
+    "revision": "b520bfd30b3b046c8f51",
+    "url": "/static/js/main.8087c39d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
